f45e63f776a9460e0c9e626b921bca0af633e577
hyojun <hjlim45678@gmail.com>
2024-03-02 21:10:28 +0900

    form삭제

--- mypage.js
+++ mypage.js
@@ -4,3 +4,18 @@ new fullpage('#fullpage', {
     scrollHorizontally: true,
     // 기타 옵션들
 });
+
+
+
+
+
+
+
+
+let bodyCheckButton = document.querySelectorAll(".body-check-area button");
+
+bodyCheckButton.forEach(button => {
+    button.addEventListener("click", (button)=>{
+        button.classList.add("like-exercise-check-button-clicked");
+    });
+  });
